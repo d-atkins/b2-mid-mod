6a6e49050642cdc2b80daaf520fca570c1668a0d
David Atkins <atki1080@gmail.com>
2020-02-14 09:31:38 -0700

    Fix typo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   validates_presence_of :name, :creation_year, :genre
-  belongs_to :movie
+  belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
 end
