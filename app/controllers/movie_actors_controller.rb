db568ead9ecaf867db143b48a7466fc1f1e68846
David Atkins <atki1080@gmail.com>
2020-02-14 11:06:01 -0700

    Add movie actors controller and create action

---
+++ app/controllers/movie_actors_controller.rb
@@ -0,0 +1,7 @@
+class MovieActorsController < ApplicationController
+  def create
+    actor = Actor.where(name: params[:name]).take
+    MovieActor.create!(movie_id: params[:movie_id], actor_id: actor.id)
+    redirect_to "/movies/#{params[:movie_id]}"
+  end
+end
