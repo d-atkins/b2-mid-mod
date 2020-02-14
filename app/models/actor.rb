f950a48cdcba8311b627d1bd6d7814bdf067b3b8
David Atkins <atki1080@gmail.com>
2020-02-14 11:38:24 -0700

    Add work_with method

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,12 @@ class Actor < ApplicationRecord
   validates_presence_of :name, :age
   has_many :movie_actors
   has_many :movies, through: :movie_actors
+
+  def worked_with
+    movies
+      .joins(:actors)
+      .where("actors.id != #{self.id}")
+      .distinct
+      .pluck("actors.name")
+  end
 end
