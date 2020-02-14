c99ab235bd3124a5a44bfd312078d84ab221efac
David Atkins <atki1080@gmail.com>
2020-02-14 11:40:38 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,16 @@ movie_2 = Movie.create!(name: 'Legend of Sal', creation_year: 1976, genre: 'acti
 movie_3 = Movie.create!(name: 'Dark Side of the Code', creation_year: 2000, genre: 'suspense', studio: studio_2)
 movie_4 = Movie.create!(name: 'Limes', creation_year: 2002, genre: 'educational', studio: studio_2)
 movie_5 = Movie.create!(name: 'Runnin Out of Ideas', creation_year: 1989, genre: 'inspiring', studio: studio_2)
+movie_6 = Movie.create!(name: "Where's Mike?", creation_year: 2016, genre: 'mystery', studio: studio_1)
 actor_1 = Actor.create!(name: 'Mike Dao', age: 22)
+actor_2 = Actor.create!(name: 'Meg Stang', age: 20)
+actor_3 = Actor.create!(name: 'Tim Tyrell', age: 53)
+actor_4 = Actor.create!(name: 'Aurora Ziobrowski', age: 36)
+actor_5 = Actor.create!(name: 'Megan McMahon', age: 37)
 actor_1.movies << [movie_1, movie_2, movie_3, movie_4, movie_5]
+movie_1.actors << [actor_2, actor_3]
+movie_2.actors << [actor_2, actor_3]
+movie_3.actors << [actor_2]
+movie_4.actors << [actor_3]
+movie_5.actors << [actor_2, actor_3]
+movie_6.actors << [actor_2, actor_3, actor_4, actor_5]
