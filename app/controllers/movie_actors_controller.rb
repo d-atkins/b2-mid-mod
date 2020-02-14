b2cd04ba8aa9a822e12cfa693366c23d620e759e
David Atkins <atki1080@gmail.com>
2020-02-14 11:08:25 -0700

    Add guard clause

--- app/controllers/movie_actors_controller.rb
+++ app/controllers/movie_actors_controller.rb
@@ -1,7 +1,7 @@
 class MovieActorsController < ApplicationController
   def create
     actor = Actor.where(name: params[:name]).take
-    MovieActor.create!(movie_id: params[:movie_id], actor_id: actor.id)
+    MovieActor.create!(movie_id: params[:movie_id], actor_id: actor.id) if actor
     redirect_to "/movies/#{params[:movie_id]}"
   end
 end
